55834a55d000a26b9ce0504dee8a056751b633dc
StepIgor <stepanow.igor@gmail.com>
2022-03-18 15:52:36 +0300

    emp chart marks is ready

--- src/Modal.js
+++ src/Modal.js
@@ -2,16 +2,21 @@ import "./CSS/Modal.css"
 import {useEffect, useState} from "react";
 import {Fade} from "react-reveal";
 import {connectionString} from "./vars";
+import DoughnutChart from "./WS_Subpages/DoughnutChart";
 
 function Modal(props) {
 
     let [modalContent, editModalContent] = useState()
     let [lastAbout, editLastAbout] = useState()
     let [lastId, editLastId] = useState();
+    let [filterStartDate, editFilterStartDate] = useState((new Date().toISOString().split('T')[0].split('-')[0] - 2)+'-01-01');
+    let [filterEndDate, editFilterEndDate] = useState(new Date().toISOString().split('T')[0]);
+    let [filterDatesWereChanged, editFilterDatesWereChanged] = useState(true);
 
     useEffect(() => {
         if (props.visible == false) return;
-        if (lastAbout == props.about && lastId == props.id) return;
+        if (lastAbout == props.about && lastId == props.id && filterDatesWereChanged == false) return;
+        editFilterDatesWereChanged(false);
         if (props.about == 'user') {
             editLastAbout('user');
             fetch(`${connectionString}/getUserInfo`, {
@@ -153,6 +158,48 @@ function Modal(props) {
                                 </table>
                             </div>
                         </div>
+                        <div className={`modal-block`}>
+                            <div>
+                                Статистика выставляемых оценок:
+                            </div>
+                            <div className={`modal-date-filter-container`}>
+                                <div>
+                                    <span>С</span>
+                                    <input type="date" onChange={(e) => {editFilterStartDate(e.target.value); editFilterDatesWereChanged(true);}} value={filterStartDate} />
+                                    <span>по</span>
+                                    <input type="date" onChange={(e) => {editFilterEndDate(e.target.value); editFilterDatesWereChanged(true);}} value={filterEndDate} />
+                                </div>
+                                <div>
+                                    <span>По семестрам:</span>
+                                    <i className={`material-icons`} onClick={() => {
+                                        if (Number.parseInt(filterEndDate.split('-')[1]) >= 2 && Number.parseInt(filterEndDate.split('-')[1]) <= 8){
+                                            editFilterEndDate(filterEndDate.split('-')[0]+'-01-31');
+                                            editFilterStartDate((filterEndDate.split('-')[0] - 1)+'-09-01');
+                                        } else {
+                                            editFilterEndDate((filterEndDate.split('-')[0] - 1)+'-08-31');
+                                            editFilterStartDate((filterEndDate.split('-')[0] - 1)+'-02-01');
+                                        }
+                                        editFilterDatesWereChanged(true);
+                                    }}>navigate_before</i>
+                                    <i className={`material-icons`} onClick={() => {
+                                        if (Number.parseInt(filterEndDate.split('-')[1]) >= 2 && Number.parseInt(filterEndDate.split('-')[1]) <= 8){
+                                            editFilterEndDate((Number.parseInt(filterEndDate.split('-')[0]) + 1)+'-01-31');
+                                            editFilterStartDate(filterEndDate.split('-')[0]+'-09-01');
+                                        } else {
+                                            editFilterEndDate(filterEndDate.split('-')[0]+'-08-31');
+                                            editFilterStartDate(filterEndDate.split('-')[0]+'-02-01');
+                                        }
+                                        editFilterDatesWereChanged(true);
+                                    }}>navigate_next</i>
+                                </div>
+                            </div>
+                            <div className={`modal-chart-and-table`}>
+                                <DoughnutChart about='employee' id={props.id} startDate={filterStartDate} endDate={filterEndDate} />
+                                <div>
+                                    {/*replace with table component*/}
+                                </div>
+                            </div>
+                        </div>
                     </div>);
                 } else {
                     editModalContent(<div className={`modal-content`}>
@@ -168,7 +215,9 @@ function Modal(props) {
     return (
         <Fade>
             <div className={`modal-wrapper ${props.visible ? 'visible' : ''}`} onClick={(e) => {
-                props.editVisible(false)
+                props.editVisible(false);
+                editFilterStartDate((new Date().toISOString().split('T')[0].split('-')[0] - 2)+'-01-01');
+                editFilterEndDate(new Date().toISOString().split('T')[0]);
             }}>
                 <div className='modal-window' onClick={(e) => {
                     e.stopPropagation()
@@ -179,7 +228,9 @@ function Modal(props) {
                             о {props.about == 'user' ? 'пользователе' : props.about == 'student' ? 'студенте' : 'сотруднике'}
                         </div>
                         <div onClick={() => {
-                            props.editVisible(false)
+                            props.editVisible(false);
+                            editFilterStartDate((new Date().toISOString().split('T')[0].split('-')[0] - 2)+'-01-01');
+                            editFilterEndDate(new Date().toISOString().split('T')[0]);
                         }}>
                             <i className={`material-icons modal-close-but`}>close</i>
                         </div>
---
+++ src/WS_Subpages/DoughnutChart.js
@@ -0,0 +1,113 @@
+import {Doughnut} from "react-chartjs-2";
+import {useEffect, useRef, useState} from "react";
+import {connectionString} from "../vars";
+import 'chart.js/auto';
+import "./CSS/DoughnutChart.css"
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+
+function DoughnutChart(props) {
+
+    let [data, editData] = useState();
+    let [errorText, editErrorText] = useState();
+
+    let [lastAbout, editLastAbout] = useState();
+    let [lastId, editLastId] = useState();
+    let [lastStartDate, editLastStartDate] = useState();
+    let [lastEndDate, editLastEndDate] = useState();
+
+    useEffect(() => {
+        if (lastAbout == props.about && lastId == props.id && lastEndDate == props.endDate && lastStartDate == props.startDate) return;
+        editLastEndDate(props.endDate);
+        editLastStartDate(props.startDate);
+        if (props.about == 'employee'){
+            editLastAbout(props.about);
+            editLastId(props.id);
+            if (props.endDate.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/) == null) return;
+            if (props.startDate.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/) == null) return;
+            fetch(`${connectionString}/getEmpMarksGroupedChartData`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    session: localStorage['session'],
+                    id: props.id,
+                    startDate: props.startDate,
+                    endDate: props.endDate
+                })
+            }).then((res) => res.json()).then((answer) => {
+                if (answer.status == 'done') {
+                    let labels = answer.marks.map((group) => {
+                        if (group.mark == 0) return 'Не зачтено';
+                        if (group.mark == 1) return 'Зачтено';
+                        return group.mark;
+                    });
+                    let data = answer.marks.map((group) => group["COUNT(mark)"]);
+                    if (data.length > 0){
+                        editErrorText(null);
+                        editData({
+                            labels: labels,
+                            datasets: [{
+                                data: data,
+                                backgroundColor: ['#f44336', '#9c27b0', '#3f51b5', '#009688', '#ff9800', '#795548']
+                            }]
+                        });
+                    } else {
+                        editData(null);
+                        editErrorText('Нет данных за указанный период')
+                    }
+                } else {
+                    editErrorText(answer.details);
+                }
+            }).catch((error) => {
+                alert('Нет связи с сервером')
+                editErrorText("Нет связи с сервером");
+            })
+        }
+    })
+
+    if (data != null){
+        return (
+            <Doughnut type="doughnut"
+                      options={{
+                          responsive: true,
+                          maintainAspectRatio: false,
+                          plugins: {
+                              datalabels: {
+                                  color: '#FFFFFF',
+                                  font: {
+                                      size: 10
+                                  },
+                                  formatter: (value, ctx) => {
+                                      let sum = 0;
+                                      let dataArr = ctx.chart.data.datasets[0].data;
+                                      dataArr.map(data => {
+                                          sum += data;
+                                      });
+                                      let percentage = "  " + value + "  \n" + (value*100 / sum).toFixed(0)+"%";
+                                      return percentage;
+                                  }
+                              },
+                              legend: {
+                                  display: true,
+                                  position: "right"
+                              },
+                          }
+                      }}
+                      data={data}
+                      plugins={[ChartDataLabels]}
+                      width={"100%"}
+                      fontSize={14}
+            />
+        )
+    } else {
+        return (
+            <div className={`chart-no-data-label`}>
+                {errorText}
+            </div>
+        )
+    }
+}
+
+export default DoughnutChart;
